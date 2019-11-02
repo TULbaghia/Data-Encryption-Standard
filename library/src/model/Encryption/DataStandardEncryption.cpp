48ba61fdd00e45ed705c4912def06ed8408cb5ee
pawelGuzek <pawelxdev@gmail.com>
2019-11-02 22:39:46 +0100

    Removed debug comments

--- library/src/model/Encryption/DataStandardEncryption.cpp
+++ library/src/model/Encryption/DataStandardEncryption.cpp
@@ -16,9 +16,6 @@ string DataStandardEncryption::encrypt(string message, Key &key) {
     is << message;
 
     vector<bitset<48>> roundKey = initializeRoundKey(key);
-//    for(int i=0; i<16; i++) {
-//        cout << "RKey" << (int)i << ": " << roundKey[i] << '\n';
-//    }
     ElectronicCodeBook(is, os, roundKey);
 
     return os.str();
@@ -34,7 +31,6 @@ string DataStandardEncryption::decrypt(string message, Key &key) {
 
     string outString = os.str();
 
-//    reverse(outString.begin(), outString.end());
     outString = helperFunctions::binaryStringToString(outString);
     outString.erase(std::find(outString.begin(), outString.end(), '\0'), outString.end());
 
@@ -51,65 +47,19 @@ void DataStandardEncryption::decrypt(string fileIn, string fileOut, Key &key) {
 
 vector<bitset<48>> DataStandardEncryption::initializeRoundKey(Key &key) {
     bitset<64> bitKey(helperFunctions::stringToBinaryString(key.getKey()));
-//    string skey = "0001001100110100010101110111100110011011101111001101111111110001";
-//    reverse(skey.begin(), skey.end());
-//
-//    bitset<64> bitKey(skey);
-
-//    cout << '\n' << "BK PT: ";
-//    for(int i=0; i<bitKey.size(); i++) {
-//        cout << bitKey[i];
-//    }
-//    cout << '\n' << "BK BS: " << bitKey << '\n';
 
     bitset<56> noParityKey = permutation(bitKey, keyParityDrop);
-
-//    cout << '\n' << "NP PT: ";
-//    for(int i=0; i<noParityKey.size(); i++) {
-//        cout << noParityKey[i];
-//    }
-//    cout << '\n' << "NP BS: " << noParityKey << '\n';
 
     bitset<28> leftKeyPart = getKeyPart(noParityKey, 0);
     bitset<28> rightKeyPart = getKeyPart(noParityKey, 28);
 
-//    cout << '\n' << "LT PT: ";
-//    for(int i=0; i<leftKeyPart.size(); i++) {
-//        cout << leftKeyPart[i];
-//    }
-//    cout << '\n' << "LT BS: " << leftKeyPart << '\n';
-
-//    cout << '\n' << "RT PT: ";
-//    for(int i=0; i<rightKeyPart.size(); i++) {
-//        cout << rightKeyPart[i];
-//    }
-//    cout << '\n' << "RT BS: " << rightKeyPart << '\n';
-
     vector<bitset<48>> roundKey;
-
     for(size_t i=0; i<16; i++) {
         shiftLeft(leftKeyPart, shiftsTable[i]);
         shiftLeft(rightKeyPart, shiftsTable[i]);
 
-//        cout << '\n' << "LT PT: ";
-//        for(int i=0; i<leftKeyPart.size(); i++) {
-//            cout << leftKeyPart[i];
-//        }
-//        cout << '\n' << "LT BS: " << leftKeyPart << '\n';
-
-//        cout << '\n' << "RT PT: ";
-//        for(int i=0; i<rightKeyPart.size(); i++) {
-//            cout << rightKeyPart[i];
-//        }
-//        cout << '\n' << "RT BS: " << rightKeyPart << '\n';
-
         bitset<56> combKey = combineKey(leftKeyPart, rightKeyPart);
-
-//        cout << "combKey: " << combKey << '\n';
-
         bitset<48> pCombinedKey = permutation(combKey, keyCompression);
-
-//        cout << "pCK: " << pCombinedKey << '\n';
 
         roundKey.push_back(pCombinedKey);
     }
@@ -178,18 +128,10 @@ void DataStandardEncryption::shiftLeft(bitset<28> &bset, const byte shiftBy) {
     for(size_t i=0; i<shiftBy; i++) {
         shifted += bset[i];
     }
-//    cout << "shifted: ";
-//    for(int i=0; i<shiftBy; i++) {
-//        cout << (int)shifted[i];
-//    }
-//    cout << '\n';
-//    cout << bset << '\n';
     bset >>= shiftBy;
-//    cout << bset << '\n';
     for(byte i=0; i<shiftBy; i++) {
         bset[bset.size()-shiftBy+i] = shifted[i];
     }
-//    cout << bset << '\n';
 }
 
 bitset<56> DataStandardEncryption::combineKey(const bitset<28> &bs1, const bitset<28> &bs2) {
@@ -210,18 +152,7 @@ bitset<64> DataStandardEncryption::combineKey(const bitset<32> &bs1, const bitse
     return nBitset;
 }
 
-
-/** DEPRECATED */
-//string DataStandardEncryption::permutation(string oldSet, const byte* array, size_t size) {
-//    string newSet;
-//    for(size_t i=0; i<size; i++) {
-//        newSet += static_cast<char>(oldSet[array[i]-1]);
-//    }
-//    return newSet;
-//}
-
 void DataStandardEncryption::ElectronicCodeBook(istream &is, ostream &os, vector<bitset<48>> roundKey) {
-
     //One extra free space for null byte due to string conversion
     char plainText[9];
     while(is.get(&plainText[0], 9)) {
@@ -231,25 +162,8 @@ void DataStandardEncryption::ElectronicCodeBook(istream &is, ostream &os, vector
             fixedPlainText += '\0';
         }
 
-        cout << fixedPlainText.size() << " - " << fixedPlainText << '\n';
-
-//        char nullLength = 8-fixedPlainText.size();
-//        for(char i=fixedPlainText.size(); i<8; i++) {
-//            if(i<7 || nullLength==1) {
-//                fixedPlainText += i;
-//            } else {
-//                fixedPlainText += nullLength;
-//            }
-//        }
-
-//        std::cout << fixedPlainText.size() << " - ";
-//        for(int i=0; i<fixedPlainText.size(); i++) {
-//            std::cout << "[" << (int)fixedPlainText[i] << "]";
-//        }
-//        std::cout << "\n";
-
-
         string text_cypherText = blockPartial(fixedPlainText, roundKey);
+
         if(!(os << text_cypherText)) {
             std::cerr << "Blad zapisu do sstring" << "\n";
         }
@@ -259,30 +173,15 @@ void DataStandardEncryption::ElectronicCodeBook(istream &is, ostream &os, vector
 }
 
 string DataStandardEncryption::blockPartial(string plainText, const vector<bitset<48>> &roundKey) {
-//    std::cout << "text: " << helperFunctions::stringToBinaryString(plainText) << " - " << plainText.size() << '\n';
     bitset<64> bitText(helperFunctions::stringToBinaryString(plainText));
-//    string pText = "0000000100100011010001010110011110001001101010111100110111101111";
-//    reverse(pText.begin(), pText.end());
-//    bitset<64> bitText(pText);
-
-//    cout << "bText: " << bitText << '\n';
-
     bitText = permutation64s(bitText, initialPermutation);
-
-//    cout << "ipbText: " << bitText << '\n';
 
     bitset<32> leftBitText = getKeyPart(bitText, 0);
     bitset<32> rightBitText = getKeyPart(bitText, 32);
 
-//    cout << "lBT: " << leftBitText << '\n';
-//    cout << "rBT: " << rightBitText << '\n';
-
     for(byte i=0; i<16; i++) {
-//        cout << "Iter: " << (int)i << '\n';
         bitset<48> extRightBitText = permutation(rightBitText, expansionFunction);
         bitset<48> xerb = xorBitset(extRightBitText, roundKey[i]);
-
-//        cout << "xerb: " << xerb << '\n';
 
         bitset<32> sBoxOp;
         for(byte j=0; j<8; j++) {
@@ -296,29 +195,16 @@ string DataStandardEncryption::blockPartial(string plainText, const vector<bitse
                 val %= k;
             }
         }
-//        cout << sBoxOp << '\n';
 
         sBoxOp = permutation32s(sBoxOp, P);
 
-//        cout << sBoxOp << '\n';
-
         leftBitText = xorBitset(sBoxOp, leftBitText);
-
-//        cout << leftBitText << '\n';
 
         swap(leftBitText, rightBitText);
     }
 
-//    cout << "lBT: " << leftBitText << '\n';
-//    cout << "rBT: " << rightBitText << '\n';
-
     bitset<64> combine = combineKey(rightBitText, leftBitText);
-
-//    cout << combine << '\n';
-
     combine = permutation64s(combine, finalPermutation);
-
-//    cout << combine << '\n';
 
     return helperFunctions::reverseString(combine.to_string());
 }
